c7caae5db76c33c311ab8e230fdd9df1215e4063
QuadmasterXLII <hastings.greer@gmail.com>
2015-10-10 12:26:01 -0400

    finished camera reader

--- camerastream.py
+++ camerastream.py
@@ -12,7 +12,7 @@ import matplotlib.cm as cm
 import time
 
 def waitCountdown():
-   for i in range(4, 1, -1):
+   for i in range(4, 0, -1):
        time.sleep(1)
        print i
 
@@ -22,8 +22,9 @@ def waitCountdown():
 pygame.camera.init()
 
 
-cam = pygame.camera.Camera("/dev/video0",(640,480))
+cam = pygame.camera.Camera("/dev/video1",(640,480))
 cam.start()
+cam.get_image()
 waitCountdown()
 lfreeimage = cam.get_image()
 
@@ -35,14 +36,20 @@ for i in range(100):
     
 cam.stop()
 
-lfreenpimage = np.frombuffer(lfreeimage.get_buffer(), dtype = np.uint8).reshape(480, 640, 3)
+lfreenpimage = np.frombuffer(lfreeimage.get_buffer(), dtype = np.uint8).reshape(480, 640, 3).astype(np.int)
 
 
 
 npvideo = ["error"] * 100
 for i in range(100):
-    npvideo[i] = np.frombuffer(npimage[i].get_buffer(), dtype = np.uint8).reshape(480, 640, 3)
+    npvideo[i] = np.frombuffer(video[i].get_buffer(), dtype = np.uint8).reshape(480, 640, 3).astype(np.int)
 
-plt.imshow(lfreenpimage - npvideo[50])
 
-plt.show()
+diff = (lfreenpimage - npvideo[50])/2 + 128
+
+diffratio = diff[:,:,2].astype(np.float) / (diff[:,:,0] + diff[:,:,1] + diff[:,:,2])
+
+if __name__ == "__main__":
+    plt.imshow(diff[:,:,2].astype(np.float) / (diff[:,:,0] + diff[:,:,1] + diff[:,:,2]))
+    plt.colorbar()
+    plt.show()
