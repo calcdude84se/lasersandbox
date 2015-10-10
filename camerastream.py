e93baa7f82d7388f4ac02c37c848ec68c3ac83c7
QuadmasterXLII <hastings.greer@gmail.com>
2015-10-10 10:16:53 -0400

    python image works

--- camerastream.py
+++ camerastream.py
@@ -8,3 +8,17 @@ import scipy.ndimage.filters as filt
 
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+
+pygame.camera.init()
+
+
+cam = pygame.camera.Camera("/dev/video0",(640,480))
+cam.start()
+
+image = cam.get_image()
+cam.stop()
+
+npimages = np.frombuffer(image.get_buffer(), dtype = np.uint8).reshape(480, 640, 3)
+
+plt.imshow(npimages)
+plt.show()
