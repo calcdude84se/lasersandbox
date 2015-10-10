bf800c347c5be77136ba9487e7cf867b86e62ec7
calcdude84se <calcdude84se@gmail.com>
2015-10-10 16:49:22 -0400

    Stuff, but transposed

--- camera_to_threedize.py
+++ camera_to_threedize.py
@@ -10,9 +10,8 @@ class HalfPlane(object):
         self.normal = normal
         self.side = side
 
-def calc_phi(xs, ys, ref_pos, ref_side, view, cameraposor, laserpos, lasertheta):
+def calc_phi(xys, ref_pos, ref_side, view, cameraposor, laserpos, lasertheta):
     cref_pos = ddd.unrotate(ref_pos - cameraposor.pos, cameraposor)
     cref_side = ddd.unrotate(ref_side, cameraposor)
-    xpos = cref_pos[1, 0]
-    ypos = -cref_pos[2, 0]
+    cpos = np.array([cref_pos[1, 0], -cref_pos[2, 0]])
     side = 
