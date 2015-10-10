bf5916c586799bfb825de7f6d46e8595973a8107
QuadmasterXLII <hastings.greer@gmail.com>
2015-10-10 10:09:49 -0400

    imports needed

---
+++ camerastream.py
@@ -0,0 +1,10 @@
+import pygame
+import pygame.camera
+from pygame.locals import *
+
+import numpy as np
+
+import scipy.ndimage.filters as filt
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
