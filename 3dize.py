2e343f23d0806e72b041de9c8891f79b901c2ad6
calcdude84se <calcdude84se@gmail.com>
2015-10-10 12:49:32 -0400

    Posor

--- 3dize.py
+++ 3dize.py
@@ -16,11 +16,11 @@ import numpy.matlib as np
 # [ cos th -sin th 0 ] [ cos ph 0 -sin ph ] [ 1      0       0 ]
 # [ sin th  cos th 0 ] [      0 1       0 ] [ 0 cos ps -sin ps ]
 # [      0       0 1 ] [ sin ph 0  cos ph ] [ 0 sin ps  cos ps ]
-def 3dize(xs, ys, view, camerapos, laserpos):
-    plane = calc_plane(laserpos)
+def 3dize(xs, ys, view, cameraposor, laserposor):
+    plane = calc_plane(laserposor)
     rays = calc_rays(xs, ys, view)
-    rot_rays = rotate(rays, camerapos)
-    3d_points = intersect(plane, camerapos, rot_rays)
+    rot_rays = rotate(rays, cameraposor)
+    3d_points = intersect(plane, cameraposor, rot_rays)
     return 3d_points
 
 class Plane(object):
@@ -28,9 +28,9 @@ class Plane(object):
         self.pos = pos
         self.normal = normal
 
-# Take a pos object and return another object with point x, y, z and normal dx,
-# dy, dz
-def calc_plane(pos):
+# Take a posor object and return another object with point x, y, z and normal
+# dx, dy, dz
+def calc_plane(posor):
     normal = np.mat([[1, 0, 0]], dtype=np.float).transpose()
-    rot_normal = rotate(normal, pos)
-    return Plane(pos.pos, rot_normal)
+    rot_normal = rotate(normal, posor)
+    return Plane(posor.pos, rot_normal)
