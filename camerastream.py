c55f39fc6b6807d986c7a61858c70ce55b5a6035
QuadmasterXLII <hastings.greer@gmail.com>
2015-10-10 10:26:43 -0400

    takes a video and compares it to an initial image

--- camerastream.py
+++ camerastream.py
@@ -9,16 +9,40 @@ import scipy.ndimage.filters as filt
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
+import time
+
+def waitCountdown():
+   for i in range(4, 1, -1):
+       time.sleep(1)
+       print i
+
+
+
+
 pygame.camera.init()
 
 
 cam = pygame.camera.Camera("/dev/video0",(640,480))
 cam.start()
+waitCountdown()
+lfreeimage = cam.get_image()
 
-image = cam.get_image()
+waitCountdown()
+
+video = ["ERROR"] * 100
+for i in range(100):
+    video[i] = cam.get_image()
+    
 cam.stop()
 
-npimages = np.frombuffer(image.get_buffer(), dtype = np.uint8).reshape(480, 640, 3)
+lfreenpimage = np.frombuffer(lfreeimage.get_buffer(), dtype = np.uint8).reshape(480, 640, 3)
 
-plt.imshow(npimages)
+
+
+npvideo = ["error"] * 100
+for i in range(100):
+    npvideo[i] = np.frombuffer(npimage[i].get_buffer(), dtype = np.uint8).reshape(480, 640, 3)
+
+plt.imshow(lfreenpimage - npvideo[50])
+
 plt.show()
