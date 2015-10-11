6f3325ced30e89c444b7cf00c4985710e35d0459
calcdude84se <calcdude84se@gmail.com>
2015-10-10 20:03:44 -0400

    More shapes

--- camera_to_threedize.py
+++ camera_to_threedize.py
@@ -32,7 +32,7 @@ def calc_phi(xys, ref_half_plane, view, cameraposor, laserpos, lasertheta):
 
 def calc_phi_points(points, laserpos, lasertheta):
     plane_line = ddd.coord(-np.sin(lasertheta), np.cos(lasertheta), 0) + laserpos
-    normals = np.cross(plane_line, points - np.array(laserpos.T)[0])
+    normals = np.cross(np.array(plane_line.T)[0], points - np.array(laserpos.T)[0])
     return calc_phi_norm(np.average(normals.transpose() / npl.norm(normals), axis = 0))
 
 def calc_phi_norm(norm):
