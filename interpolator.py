2347edd77089b82386660524aeb8a4b2e3b31ca0
QuadmasterXLII <hastings.greer@gmail.com>
2015-10-10 20:59:44 -0400

    finished

--- interpolator.py
+++ interpolator.py
@@ -1,12 +1,13 @@
 import numpy as np
 import scipy.spatial
+import scipy.ndimage.filters as filt
 
 import matplotlib.pyplot as plt
 
 def interpolate(xmesh, ymesh, pointcloud):
     shape = xmesh.shape
    
-    mytree = scipy.spatial.cKDTree(pointcloud.transpose()[:,:2])
+    mytree = scipy.spatial.cKDTree(pointcloud[:,:2])
     
     xflat = xmesh.flatten()
     yflat = ymesh.flatten()
@@ -24,19 +25,19 @@ def interpolate(xmesh, ymesh, pointcloud):
 
 
 if __name__ == "__main__":
-    xarray = np.linspace(-5, 5, 100)
-    yarray = np.linspace(-5, 5, 100)
+    xarray = np.linspace(-5, 5, 400)
+    yarray = np.linspace(-5, 5, 400)
     
     xmesh, ymesh = np.meshgrid(xarray, yarray)
     
-    xpoint = np.random.ranf(40)*10 -5
-    ypoint = np.random.ranf(40)*10 -5
+    xpoint = np.random.ranf(400)*10 -5
+    ypoint = np.random.ranf(400)*10 -5
     zpoint = xpoint**2 + ypoint **2
     
     pointcloud = np.array([xpoint, ypoint, zpoint]).transpose()
     
     zarray , mytree = interpolate(xmesh, ymesh, pointcloud)
-    
+    zarray = filt.gaussian_filter(zarray, 4)
     plt.imshow(zarray)
     plt.show()
     
