340963ff5ddab567efaa6789987d7b02701406b7
Alvin Zhang <alvn.zng@gmail.com>
2015-10-11 01:58:55 -0400

    reduced size

--- toMinecraft.py
+++ toMinecraft.py
@@ -11,7 +11,7 @@ def normalize(numpyArray):
     m = np.amax(numpyArray)
     for x in np.arange(0, numpyArray.shape[0]):
         for y in np.arange(0, numpyArray.shape[1]):
-            numpyArray[x][y] = 200*numpyArray[x][y]/m
+            numpyArray[x][y] = 50*numpyArray[x][y]/m
     return numpyArray
 
 ##a = normalize(a)
@@ -21,10 +21,14 @@ def createString(numpyArray):
     numpyArray = normalize(numpyArray)
     string = ''''''
     string += 'tp quadmasterxlii '+str(numpyArray.shape[0]/2.)+' 256 '+str(numpyArray.shape[1]/2.)+'\n'
+##    m = np.amin(numpyArray)
+##    for x in np.arange(0, numpyArray.shape[0]):
+##        for y in np.arange(0, numpyArray.shape[1]):
+##            numpyArray[x][y] = numpyArray[x][y] - m
     ##erase drawing
     for x in range(0, int(numpyArray.shape[0]/10.)+1):
         for y in range(0, int(numpyArray.shape[1]/10.)+1):
-            string += 'fill '+str(10*x)+' '+str(0)+' '+str(10*y)+' '+str(10*x+10)+' '+str(200)+' '+str(10*y+10)+' minecraft:air \n'
+            string += 'fill '+str(10*x)+' '+str(0)+' '+str(10*y)+' '+str(10*x+10)+' '+str(50)+' '+str(10*y+10)+' minecraft:air \n'
     ##fill drawing
     for x in np.arange(0, numpyArray.shape[0]):
         for y in np.arange(0, numpyArray.shape[1]):
